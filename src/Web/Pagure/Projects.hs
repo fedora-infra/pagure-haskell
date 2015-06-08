003c08ba94695935258358798d6b609d0d09f27d
Ricky Elrod <ricky@elrod.me>
2015-06-08 06:56:19 -0400

    issue and issueFork functions + types + lenses
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Web/Pagure/Projects.hs
+++ src/Web/Pagure/Projects.hs
@@ -48,3 +48,41 @@ gitTagsFork :: Username -> Repo -> PagureT [Tag]
 gitTagsFork u r = do
   resp <- pagureGet ("fork/" ++ u ++ "/" ++ r ++ "/git/tags")
   return $ resp ^.. responseBody . key "tags" . values . _String
+
+-- | Access the @/[repo]/issues@ endpoint.
+--
+-- Traversing the response is particularly nice because we define lenses for all
+-- of the fields you get back. See the example below.
+--
+-- Example:
+--
+-- @
+-- >>> import Web.Pagure
+-- >>> let pc = PagureConfig "https://pagure.io" Nothing
+-- >>> issues <- runPagureT (issues "pagure") pc
+-- >>> λ> issues ^. issueList . to head . comments . to head . comment
+-- "wat?"
+-- @
+issues :: Repo -> PagureT IssueResponse
+issues r = do
+  resp <- asJSON =<< pagureGet (r ++ "/issues")
+  return (resp ^. responseBody)
+
+-- | Access the @/fork/[username]/[repo]/issues@ endpoint.
+--
+-- Traversing the response is particularly nice because we define lenses for all
+-- of the fields you get back. See the example below.
+--
+-- Example:
+--
+-- @
+-- >>> import Web.Pagure
+-- >>> let pc = PagureConfig "https://pagure.io" Nothing
+-- >>> issues <- runPagureT (issuesFork "codeblock" "pagure") pc
+-- >>> λ> issues ^. issueList . to head . comments . to head . comment
+-- "wat?"
+-- @
+issuesFork :: Username -> Repo -> PagureT IssueResponse
+issuesFork u r = do
+  resp <- asJSON =<< pagureGet ("fork/" ++ u ++ "/" ++ r ++ "/issues")
+  return (resp ^. responseBody)
--- src/Web/Pagure/Types.hs
+++ src/Web/Pagure/Types.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 -----------------------------------------------------------------------------
 -- |
@@ -12,8 +15,11 @@
 ----------------------------------------------------------------------------
 module Web.Pagure.Types where
 
+import Control.Applicative
 import Control.Lens
+import Control.Monad (mzero)
 import Control.Monad.Trans.Reader
+import Data.Aeson
 import qualified Data.Text as T
 
 -- | Our 'PagureT' type which is really a 'ReaderT' with 'IO' as its base. For
@@ -40,6 +46,101 @@ data PagureConfig = PagureConfig {
   } deriving (Eq, Show)
 makeLenses ''PagureConfig
 
+type Content = String
+type Private = Bool
 type Repo = String
 type Tag = T.Text
+type Title = String
 type Username = String
+
+-- | Response type for the 'Web.Pagure.Projects.issues' function.
+data IssueResponse =
+  IssueResponse { _issueResponseArgs :: IssueArgs
+                , _issueResponseIssueList :: [Issue]
+                } deriving (Eq, Show)
+
+data IssueArgs =
+  IssueArgs { _issueArgsAssignee :: Maybe String
+            , _issueArgsAuthor :: Maybe String
+            , _issueArgsStatus :: Maybe String
+            , _issueArgsTagList  :: [Tag]
+            } deriving (Eq, Show)
+
+data Issue =
+  Issue { _issueAssignee :: Maybe IssueUser
+        , _issueBlocks :: [String]    -- TODO: is [String] correct?
+        , _issueComments :: [IssueComment]
+        , _issueContent :: String
+        , _issueDateCreated :: String
+        , _issueDepends :: [String]   -- TODO: is [String] correct?
+        , _issueId :: Integer
+        , _issuePrivate :: Bool
+        , _issueStatus :: String
+        , _issueTagList :: [Tag]
+        , _issueTitle :: String
+        , _issueUser :: IssueUser
+        } deriving (Eq, Show)
+
+data IssueUser =
+  IssueUser { _issueUserFullname :: String
+            , _issueUserName :: String
+            } deriving (Eq, Show)
+
+data IssueComment =
+  IssueComment { _issueCommentComment :: String
+               , _issueCommentDateCreated :: String
+               , _issueCommentId :: Integer
+               , _issueCommentParent :: Maybe Integer
+               , _issueCommentUser :: IssueUser
+               } deriving (Eq, Show)
+
+makeFields ''IssueResponse
+makeFields ''IssueArgs
+makeFields ''Issue
+makeFields ''IssueUser
+makeFields ''IssueComment
+
+instance FromJSON IssueResponse where
+  parseJSON (Object x) = IssueResponse <$>
+                         x .: "args"
+                     <*> x .: "issues"
+  parseJSON _            = mzero
+
+instance FromJSON IssueArgs where
+  parseJSON (Object x) = IssueArgs <$>
+                         x .: "assignee"
+                     <*> x .: "author"
+                     <*> x .: "status"
+                     <*> x .: "tags"
+  parseJSON _            = mzero
+
+instance FromJSON Issue where
+  parseJSON (Object x) = Issue <$>
+                         x .: "assignee"
+                     <*> x .: "blocks"
+                     <*> x .: "comments"
+                     <*> x .: "content"
+                     <*> x .: "date_created"
+                     <*> x .: "depends"
+                     <*> x .: "id"
+                     <*> x .: "private"
+                     <*> x .: "status"
+                     <*> x .: "tags"
+                     <*> x .: "title"
+                     <*> x .: "user"
+  parseJSON _            = mzero
+
+instance FromJSON IssueUser where
+  parseJSON (Object x) = IssueUser <$>
+                         x .: "fullname"
+                     <*> x .: "name"
+  parseJSON _            = mzero
+
+instance FromJSON IssueComment where
+  parseJSON (Object x) = IssueComment <$>
+                         x .: "comment"
+                     <*> x .: "date_created"
+                     <*> x .: "id"
+                     <*> x .: "parent"
+                     <*> x .: "user"
+  parseJSON _            = mzero
