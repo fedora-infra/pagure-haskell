3a810778138205b42dc5e13f66fba8bca4ea36fb
Ricky Elrod <ricky@elrod.me>
2015-06-24 03:32:53 -0400

    grr
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- upload-docs.sh
+++ upload-docs.sh
@@ -1,3 +1,5 @@
 standalone-haddock --package-db /home/ricky/.ghc/x86_64-linux-7.10.1/package.conf.d/ -o doc . --hyperlink-source
+echo '<!doctype html>' > doc/index.html
+echo '<html><head><meta http-equiv="refresh" content="0; url=./pagure/" /></head></html>' >> doc/index.html
 rsync --delete --partial --progress -avzre ssh doc/ fedorapeople.org:public_html/pagure-haskell
 rm -rf doc
